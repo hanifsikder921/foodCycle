b7153df7c4ada31dc07799fca4d01350b950d236
Md Hanif Sikder <mdhanifsikderpersonal@gmail.com>
2025-07-15 08:03:35 +0600

    Project Complete and Deploy to Firebase

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://food-cycle-server-blond.vercel.app',
 });
 
 const useAxiosSecure = () => {
